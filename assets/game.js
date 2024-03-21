08a557b0ac190ac72e03f96093252a353aed43fc
distractabee <jssclgh@gmail.com>
2024-03-21 16:41:19 -0500

    started writing javascript variables and functions to make the quiz work, adjusted styling

--- assets/game.js
+++ assets/game.js
@@ -28,8 +28,51 @@ const questions = [
     },
 ]
 
-var timeLeft = 75
+// variables to fill in with the question and answer choices text
+const introEl = document.getElementById("intro");
+const quizConEl = document.getElementById("quizContainer");
+const questionEl = document.querySelector(".questionText");
+const btnAEl = document.getElementById("buttonA");
+const btnBEl = document.getElementById("buttonB");
+const btnCEl = document.getElementById("buttonC");
+const btnDEl = document.getElementById("buttonD");
+
+// game countdown
+var score;
+var secondsLeft = 75;
+var qIndex = 0;
 
 function countdown() {
-    
+    var timerText = document.getElementById("timer");
+    var timeLeft = setInterval(function () {
+        secondsLeft--;
+        timerText.textContent = `Time Left: ${secondsLeft}`;
+        if (secondsLeft <= 0) {
+            clearInterval(timeLeft);
+            gameOver();
+        }
+    }, 1000);
+};
+
+const startBtn = document.getElementById("startButton");
+
+function startGame() {
+    score = 0;
+    countdown();
+    introEl.setAttribute("style", "display: none");
+    quizConEl.setAttribute("style", "display: block");
+    askAway();
+};
+
+// loads the questions into the text containers on the page
+function askAway() {
+    questionEl.textContent = questions[qIndex].question;
+    btnAEl.textContent = questions[qIndex].choices[0];
+    btnBEl.textContent = questions[qIndex].choices[1];
+    btnCEl.textContent = questions[qIndex].choices[2];
+    btnDEl.textContent = questions[qIndex].choices[3];
 }
+
+// Add event listener that starts the game
+
+startBtn.addEventListener("click", startGame);
