749ee7930faf53d217df03b68ccaf8eea2f02e44
distractabee <jssclgh@gmail.com>
2024-03-21 14:40:37 -0500

    adding questions for the quiz and startin to write functions

---
+++ assets/game.js
@@ -0,0 +1,35 @@
+// Questions for the quiz:
+
+const questions = [
+    {
+        question: "Which of the following is not a commonly used data type?",
+        choices: ["Boolean", "Integer", "Number", "String"],
+        answer: "Number"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {
+        question: "What kind of loop runs a section of code repeatedly until a certain condition has been satisfied?",
+        choices: ["If Loop", "For Loop", "While Loop", "None of These"],
+        answer: "For Loop"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed in: ",
+        choices: ["Parentheses", "Curly Brackets", "Square Brackets", "Quotation Marks"],
+        answer: "Parentheses"
+    },
+    {
+        question: "A very useful tool for debugging code is: ",
+        choices: ["console.log()", "JavaScript", "Phoning a Friend", "For Loops"],
+        answer: "console.log()"
+    },
+]
+
+var timeLeft = 75
+
+function countdown() {
+    
+}
