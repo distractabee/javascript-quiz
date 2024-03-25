3212627925ee31ea5e406bfd1018f718a39adc5f
distractabee <jssclgh@gmail.com>
2024-03-25 18:01:26 -0500

    working on the function to end the game

--- assets/game.js
+++ assets/game.js
@@ -54,6 +54,11 @@ function countdown() {
     }, 1000);
 };
 
+function gameOver() {
+    score = secondsLeft;
+    
+};
+
 const startBtn = document.getElementById("startButton");
 
 function startGame() {
@@ -75,3 +80,41 @@ function askAway() {
 
 // Add event listener that starts the game
 startBtn.addEventListener("click", startGame);
+
+// Add event listeners to the answer choices
+btnAEl.addEventListener("click", checkA);
+btnBEl.addEventListener("click", checkB);
+btnCEl.addEventListener("click", checkC);
+btnDEl.addEventListener("click", checkD);
+
+function checkA() {checkAnswer(0)}
+function checkB() {checkAnswer(1)}
+function checkC() {checkAnswer(2)}
+function checkD() {checkAnswer(3)}
+
+// Check for correct answer, move to next question
+
+
+function checkAnswer(answer) {
+    if (questions[qIndex].answer === questions[qIndex].choices[answer]) {
+        secondsLeft = secondsLeft + 5
+    } else {
+        secondsLeft = secondsLeft - 10
+    }
+    nextQuestion();
+}
+
+function nextQuestion() {
+    if (qIndex < 4 ) {
+        qIndex++;
+        askAway();
+    } else {
+        // set time left as score and end game
+        gameOver()
+    }
+}
+
+
+// function to end game
+// prompt to enter high score
+// save high score to local storage and create new line on high-score.html
